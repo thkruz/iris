0843bbaa4897baa63697e8936d884cc0117eb14e
filmo003 <collin.gilmore47@gmail.com>
2022-04-21 12:36:03 -0400

    build: :card_file_box: Created migrations for signals and users dbs

---
+++ api/db/migrations/03_create_signals_foreign.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('signals', table => {
+    table.foreign('user_id').references('users.id').onDelete('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  knex.schema.table('signals', table => {
+      table.dropForeign('user_id');
+    });
+};
