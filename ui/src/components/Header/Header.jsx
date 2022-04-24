1a37ebd0a2e80f592841f07329807e3e585bbd91
Huf <bjhufstetler@gmail.com>
2022-04-24 10:07:14 -0700

    chore: :sparkles: adds name and icon to metadata and browser tab

---
+++ ui/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// MUI App Bar: https://mui.com/material-ui/react-app-bar/
+
+export const Header = () => {
+
+    return (
+      <>
+        Header 
+      </>
+    );
+  }
+  
