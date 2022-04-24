4d58cd3cb224ac7cd1b88c9b85a2e6a564815b29
Huf <bjhufstetler@gmail.com>
2022-04-24 11:38:22 -0700

    fix: comments out unused dependency

--- ui/src/themes/AstroTheme.js
+++ ui/src/themes/AstroTheme.js
@@ -1,4 +1,4 @@
-import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+//import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 
 export const AstroTheme = {
   palette: {
