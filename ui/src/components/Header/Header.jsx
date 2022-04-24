e8efcb1d0a28b02ceb1594dbf3c0a6e1f830f645
Huf <bjhufstetler@gmail.com>
2022-04-24 11:34:27 -0700

    feat: :building_construction: adds mui based header

--- ui/src/components/Header/Header.jsx
+++ ui/src/components/Header/Header.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
-
-// MUI App Bar: https://mui.com/material-ui/react-app-bar/
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Logout from '@mui/icons-material/Logout';
+import './Header.css'
 
 export const Header = () => {
+  const auth = true; //TODO: get auth context
 
+  const handleLogout = () => {
+    console.log("logging out")
+  }
     return (
       <>
-  
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <img src='/patch.png' alt='patch.png' height='80px'></img>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Iris Space Electronic Warfare Sandbox
+              </Typography>
+              {auth &&
+                <IconButton
+                  size="large"
+                  aria-labels="logout"
+                  onClick={handleLogout}
+                  color="inherit"
+                  >
+                    <Logout />
+                </IconButton>
+              }
+            </Toolbar>
+          </AppBar>
+        </Box>
       </>
     );
   }
