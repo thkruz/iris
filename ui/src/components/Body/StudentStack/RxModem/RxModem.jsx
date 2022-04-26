08ba749fee54a52e3581bc99c0a94090c6bf29bf
Huf <bjhufstetler@gmail.com>
2022-04-25 22:12:52 -0700

    chore: :sparkles: adds AstroTheme to RxModem

--- ui/src/components/Body/StudentStack/RxModem/RxModem.jsx
+++ ui/src/components/Body/StudentStack/RxModem/RxModem.jsx
@@ -1,70 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import { Box, Button, Typography } from '@mui/material';
 import './RxModem.css'
+import { AstroTheme } from '../../../../themes/AstroTheme';
 
-export const RxModem = () => {
+export const RxModem = ({ unit }) => {
     //TODO: modem buttons, update state, video, 
-    
-    const RxData = [
-        {unit: 1, number: 1, operational: true, id_antenna: 1, frequency: 1100, bandwidth: 25, modulation: '8PSK', fec: '3/5'},
-        {unit: 1, number: 2, operational: true, id_antenna: 1, frequency: 1200, bandwidth: 35, modulation: 'BPSK', fec: '1/2'},
-        {unit: 1, number: 3, operational: true, id_antenna: 1, frequency: 1300, bandwidth: 45, modulation: 'QPSK', fec: '3/4'},
-        {unit: 1, number: 4, operational: true, id_antenna: 1, frequency: 1400, bandwidth: 55, modulation: '16PSK', fec: '1/2'}
+    const theme = AstroTheme
+    const tmpRxData = [
+        {unit: 1, number: 1, operational: true, antenna: 1, frequency: 1100, bandwidth: 25, modulation: '8PSK', fec: '3/5'},
+        {unit: 1, number: 2, operational: true, antenna: 1, frequency: 1200, bandwidth: 35, modulation: 'BPSK', fec: '1/2'},
+        {unit: 1, number: 3, operational: true, antenna: 1, frequency: 1300, bandwidth: 45, modulation: 'QPSK', fec: '3/4'},
+        {unit: 1, number: 4, operational: true, antenna: 1, frequency: 1400, bandwidth: 55, modulation: '16PSK', fec: '1/2'}
     ];
-
+    const [rxData] = useState(tmpRxData);
+    const [activeModem, setActiveModem] = useState(0);
+    
     // Styles
     const sxCase = {
         flexGrow: 1,
-        backgroundColor: 'gray',
+        width: '600px',
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: '10px',
+        border: '1px solid black',
         display:'grid', 
         gridTemplateColumns: '1fr 1fr 4fr 3fr 5fr',
-        alignItems: 'center'
+        justifyContent: 'space-between'
     };
     const sxCaseId = {
-        transform: 'rotate(-90deg)',
-        textAlign: 'center',
-        backgroundColor: 'yellow'
+        color: 'white',
+        margin: '8px',
+        textAlign: 'center'
     };
     const sxModemButtonBox = {
-        backgroundColor: 'black'
+        backgroundColor: theme.palette.primary.light,
+        borderRadius: '5px',
+        boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)'
     };
     const sxModemButton = {
-        backgroundColor: 'red', 
-        border: '1px solid black', 
+        backgroundColor: theme.palette.primary.light,
+        boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
+        color: 'black',
+        margin: '8px',
+        cursor: 'pointer'
+    };
+    const sxModemButtonActive = {
+        backgroundColor: theme.palette.primary.dark,
+        boxShadow: '0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)',
+        color: 'white',
         width: '1em',
-        margin: '7px'
-    };
-    const sxValueTable = {
-        margin: '10px',
-        display: 'flex', 
-        flexDirection: 'column', 
-        border: '1px solid black'
-    };
-    const sxValueTableHead = {
-        textAlign: 'center', 
-        backgroundColor: 'purple'
-    };
-    const sxValuesBox = {
-        backgroundColor: 'green',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'end',
-        justifyContent: 'flex-end',
-        border: '1px solid black'
-    };
-    const sxValuesRow = {
-        display: 'grid',
-        gridTemplateColumns: '95px 95px',
-        textAlign: 'right'
+        margin: '8px',
+        outline: 'none'
     };
     const sxValues = {
         fontWeight: 'bold',
         textDecoration: 'underline'
     };
     const sxInputBox = {
-        margin: '10px',
-        backgroundColor: 'blue',
+        backgroundColor: theme.palette.primary.main,
+        margin: '8px',
+        borderRadius: '4px',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'end',
@@ -72,10 +67,17 @@ export const RxModem = () => {
     };
     const sxInputRow = {
         display: 'grid',
-        gridTemplateColumns: '95px 75px 50px', 
+        gridTemplateColumns: '80px 80px 80px', 
         textAlign: 'right', 
-        margin: '5px'
+        margin: '2px'
     };
+    const sxInputApply = {
+        backgroundColor: theme.palette.primary.light,
+        boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
+        color: 'black',
+        margin: '8px',
+        cursor: 'pointer'
+    }
     const sxVideo = {
         margin: '10px',
         border: '1px solid black',
@@ -83,110 +85,74 @@ export const RxModem = () => {
         width: '200px',
         height: '200px'
     }
+
      // Modem Case Id
+     const sidebar = [];
+     ['U','N','I','T'].forEach((x, index) => {
+         sidebar.push(<Typography key={index} sx={{color: 'black'}}>{x}</Typography>)
+     });
      const RxCaseId = () => (
-            <Typography sx={sxCaseId}>{RxData[0].unit}</Typography>
+        <Box sx={sxCaseId}>
+            {sidebar}
+            <br></br>
+            <Typography>{unit}</Typography>
+        </Box>
     )
 
     // Modem selector buttons
     const RxModemButtonBox = () => (
         <Box sx={sxModemButtonBox}>
-            {RxData.map((x, index) => (
+            {rxData.map((x, index) => (
                 <RxModemButton key={index} modem={x.number} />
             ))}
         </Box>
     );
     const RxModemButton = ({ modem }) => (
-        <Button sx={sxModemButton}
-        onClick={e => handleModemButtonClick(parseInt(e.target.innerText))}>
+        <Button sx={modem === activeModem + 1 ? sxModemButtonActive : sxModemButton}
+        onClick={e => {setActiveModem(parseInt(e.target.innerText) - 1); console.log(activeModem)}}>
                 {modem}
         </Button>
     );
-    const handleModemButtonClick = ( modem ) => {
-        console.log(modem)
-    };
+    RxModemButton.propTypes = {modem: PropTypes.number};
+    
 
     // Modem User Inputs
+    const inputs = [];
+    ['Antenna', 'Frequency', 'Bandwidth', 'Modulation', 'FEC'].forEach((x, index) => {
+        inputs.push(
+            <Box key={index} sx={sxInputRow}>
+                <label htmlFor={x}>{x}</label>
+                <input name={x} type='text' placeholder={rxData[activeModem][x.toLowerCase()]}></input>
+                <Typography sx={sxValues}>{rxData[activeModem][x.toLowerCase()]}</Typography>
+            </Box>
+        )
+    });
+
     const RxModemInput = () => (
         <Box sx={sxInputBox}>
-            <Box sx={sxInputRow}>
-                <label htmlFor='antenna'>Antenna</label>
-                <input name='antenna' type='text' value={RxData[0].id_antenna}></input>
-            </Box>
-            <Box sx={sxInputRow}>
-                <label htmlFor='frequency'>Fequency</label>
-                <input name='frequency' type='text' value={RxData[0].frequency}></input>
-                <Typography>MHz</Typography>
-            </Box>
-            <Box sx={sxInputRow}>
-                <label htmlFor='bandwidth'>Bandwidth</label>
-                <input name='bandwidth' type='text' value={RxData[0].bandwidth}></input>
-                <Typography>MHz</Typography>
-            </Box>
-            <Box sx={sxInputRow}>
-                <label htmlFor='modulation'>Modulation</label>
-                <input name='modulation' type='text' value={RxData[0].modulation}></input>
-            </Box>
-            <Box sx={sxInputRow}>
-                <label htmlFor='fec'>FEC</label>
-                <input name='fec' type='text' value={RxData[0].fec}></input>
-            </Box>
-            <Box sx={sxInputRow}>
-                <div></div>
-                <Button onClick={e => console.log(e.target.innerText)}>Apply</Button>
-            </Box>
-            
+            {inputs}    
+            <Button sx={sxInputApply} onClick={e => console.log(e.target.innerText)}>Apply</Button>
         </Box>
     );
-
-    // Current Modem Values
-    const RxModemValues = () => (
-        <Box sx={sxValueTable}>
-            <Typography sx={sxValueTableHead}>Current Values</Typography>
-            <Box sx={sxValuesBox}>
-                <Box sx={sxValuesRow}>
-                    <Typography>Antenna: </Typography>
-                    <Typography sx={sxValues}>{RxData[1].id_antenna}</Typography>
-                </Box>
-                <Box sx={sxValuesRow}>
-                    <Typography>Frequency: </Typography>
-                    <Typography sx={sxValues}>{`${RxData[1].frequency} MHz`}</Typography>
-                </Box>
-                <Box sx={sxValuesRow}>
-                    <Typography>Bandwidth: </Typography>
-                    <Typography sx={sxValues}>{`${RxData[1].bandwidth} MHz`}</Typography>
-                </Box>
-                <Box sx={sxValuesRow}>
-                    <Typography>Modulation: </Typography>
-                    <Typography sx={sxValues}>{RxData[1].modulation}</Typography>
-                </Box>    
-                <Box sx={sxValuesRow}>
-                    <Typography>FEC: </Typography>
-                    <Typography sx={sxValues}>{RxData[1].fec}</Typography>
-                </Box>
-            </Box>
-        </Box>
-    )
     
     const RxVideo = () => (
         <Box sx={sxVideo}>
             Video Goes Here
         </Box>
-    )
+    );
 
     return(
         <>
-                <Box sx={sxCase}>
-                    <RxCaseId />
-                    <RxModemButtonBox />
-                    <RxModemValues />
-                    <RxModemInput />
-                    <RxVideo />
-                </Box>
+            <Box sx={sxCase}>
+                <RxCaseId />
+                <RxModemButtonBox />
+                <RxModemInput />
+                <RxVideo />
+            </Box>
         </>
     )
 }
 
 RxModem.propTypes = {
-    modem: PropTypes.integer
+    unit: PropTypes.number
 }
