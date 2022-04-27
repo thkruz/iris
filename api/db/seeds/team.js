562c4e8e9cad8f0a08bcab7552035127d72a274f
Huf <bjhufstetler@gmail.com>
2022-04-27 14:21:43 -0700

    chore: :card_file_box: adds seed for team table

---
+++ api/db/seeds/team.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('team').del()
+  await knex('team').insert([
+    {id: 1, name: 'Persephone'},
+    {id: 2, name: 'Sisyphus'},
+    {id: 3, name: 'Tartarus'},
+    {id: 4, name: 'Zagreus'}
+  ]);
+};
