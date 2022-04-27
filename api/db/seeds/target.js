e990e91f25a1e701b5bfe990703654de0bbe9d37
Huf <bjhufstetler@gmail.com>
2022-04-27 14:27:31 -0700

    chore: changes a satellite name

--- api/db/seeds/target.js
+++ api/db/seeds/target.js
@@ -6,15 +6,15 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('target').del()
   await knex('target').insert([
-    {id: 1, name: 'WILL 1-AM'},
-    {id: 2, name: 'HUFUHF FO'},
-    {id: 3, name: 'ARKE 3G'},
-    {id: 4, name: 'HASHCOMM 7'},
-    {id: 5, name: 'NYXSAT'},
-    {id: 6, name: 'RASCAL'},
-    {id: 7, name: 'AURORA 2B'},
-    {id: 8, name: 'MERCURY PAWN'},
-    {id: 9, name: 'AUXO STAR'},
-    {id: 10, name: 'ENYO'},
+    {id: 1, name: 'ARKE 3G'},
+    {id: 2, name: 'AURORA 2B'},
+    {id: 3, name: 'AUXO STAR'},
+    {id: 4, name: 'ENYO'},
+    {id: 5, name: 'HASHCOMM 7'},
+    {id: 6, name: 'HUF UHF FO'},
+    {id: 7, name: 'MERCURY PAWN'},
+    {id: 8, name: 'NYXSAT'},
+    {id: 9, name: 'RASCAL'},
+    {id: 10, name: 'WILL 1-AM'},
   ]);
 };
