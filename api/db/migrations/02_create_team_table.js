ba89c1c21a044d183282c489dd09f028fa6765e6
filmo003 <collin.gilmore47@gmail.com>
2022-04-27 18:13:44 -0400

    remove server_id

--- api/db/migrations/02_create_team_table.js
+++ api/db/migrations/02_create_team_table.js
@@ -6,8 +6,6 @@ exports.up = function(knex) {
   return knex.schema.createTable('team', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
-    table.integer('server_id').notNullable();
-    table.foreign('server_id').references('server.id');
   });
 };
 
