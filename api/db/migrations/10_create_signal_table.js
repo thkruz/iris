d84fc54b4959ad7c3df6451d9c53d83379a17e00
filmo003 <collin.gilmore47@gmail.com>
2022-04-27 15:22:25 -0400

    feat: :card_file_box: create spec_a, signal, and inject tables

---
+++ api/db/migrations/10_create_signal_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('signal', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.integer('server_id').notNullable();
+        table.foreign('server_id').references('server.id');
+        table.integer('team_id').notNullable();
+        table.foreign('team_id').references('team.id');
+        table.integer('target_id').notNullable();
+        table.foreign('target_id').references('target.id');
+        table.double('frequency').notNullable();
+        table.string('modulation').notNullable();
+        table.string('fec').notNullable();
+        table.string('feed').notNullable();
+        table.boolean('operational').defaultTo(false);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('signal');
+};
