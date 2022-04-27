e188803ef5b6d4a255e01be00c9eb5c4cc829488
Huf <bjhufstetler@gmail.com>
2022-04-27 14:56:10 -0700

    chore: adds offset to target data

--- api/db/seeds/target.js
+++ api/db/seeds/target.js
@@ -6,15 +6,15 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('target').del()
   await knex('target').insert([
-    {id: 1, name: 'ARKE 3G'},
-    {id: 2, name: 'AURORA 2B'},
-    {id: 3, name: 'AUXO STAR'},
-    {id: 4, name: 'ENYO'},
-    {id: 5, name: 'HASHCOMM 7'},
-    {id: 6, name: 'HUF UHF FO'},
-    {id: 7, name: 'MERCURY PAWN'},
-    {id: 8, name: 'NYXSAT'},
-    {id: 9, name: 'RASCAL'},
-    {id: 10, name: 'WILL 1-AM'},
+    {id: 1, name: 'ARKE 3G', offset: 400},
+    {id: 2, name: 'AURORA 2B', offset: 450},
+    {id: 3, name: 'AUXO STAR', offset: 420},
+    {id: 4, name: 'ENYO', offset: 300},
+    {id: 5, name: 'HASHCOMM 7', offset: 365},
+    {id: 6, name: 'HUF UHF FO', offset: 210},
+    {id: 7, name: 'MERCURY PAWN', offset: 150},
+    {id: 8, name: 'NYXSAT', offset: 250},
+    {id: 9, name: 'RASCAL', offset: 120},
+    {id: 10, name: 'WILL 1-AM', offset: 345},
   ]);
 };
