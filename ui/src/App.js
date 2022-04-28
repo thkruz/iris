6c2b4ee7a881a80523af0f9134dd3e9d283cbba7
Brett Peters <peters_brett@hotmail.com>
2022-04-28 09:12:40 -0700

    routes built with blank login page route

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,27 +1,36 @@
-import React, { useEffect, useState} from 'react';
-import config from './config';
-import { Header, Body } from './components';
+import React, { useEffect, useState } from "react";
+import config from "./config";
+import { Header, Body } from "./components";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 function App() {
-
-  let [names, setNames] = useState([ ]);
+  let [names, setNames] = useState([]);
 
   useEffect(() => {
     fetch(ApiUrl + "/authors")
-      .then(response => response.json())
-      .then(data => setNames(data))
-      .catch(err => console.log(err))
+      .then((response) => response.json())
+      .then((data) => setNames(data))
+      .catch((err) => console.log(err));
   }, []);
-
 
   return (
     <div>
-      <Header />
-      <Body />
-      App is running - good work: 
-      { names.map(author => author.firstName + " ")}
+      <Router>
+        <Header />
+        <Body />
+        <div>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            {/* <Route path="login" element={<Login />} /> */}
+            <Route path="/student" element={<h1>Student</h1>} />
+            <Route path="/instructor" element={<h1>Instructor</h1>} />
+          </Routes>
+        </div>
+        App is running - good work:
+        {names.map((author) => author.firstName + " ")}
+      </Router>
     </div>
   );
 }
---
+++ ui/src/components/Login/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+import config from "./config";
+
+const Login = () => {
+  return <div>Login Page</div>;
+};
+export default Login;
