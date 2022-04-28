cb1f0852ec95216b36e995d3dcf115d76dbef344
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-04-28 11:04:09 -0400

    fix: :bug: fix naming convention change and port mismatch

--- api/src/app.js
+++ api/src/app.js
@@ -9,91 +9,97 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (request, response) => {
-    response.set("Access-Control-Allow-Origin", "*");
-    response.status(200).send('App root route running');
+  response.set('Access-Control-Allow-Origin', '*');
+  response.status(200).send('App root route running');
 });
 
 app.get('/authors', (request, response) => {
-    knex('app_authors')
-        .then(data => {
-            response.status(200).send(data.JSON())
-        });
+  knex('app_authors').then((data) => {
+    if (data.length === 0) {
+      response.status(404).send('No authors found');
+    } else {
+      response.status(200).send(data);
+    }
+  });
 });
 
 app.get('/data/:table_name', (request, response) => {
-    if(request.query.id !== undefined) {
-        knex(request.params.table_name)
-        .select('*')
-        .where('id', request.query.id)
-        .then(responseData => {
-            response.status(200).send(responseData);
-        })
-        .catch(error => {
-            response.status(500).send(error);
-        });
-    } else if (request.query.server_id !== undefined) {
-        knex(request.params.table_name)
-        .select('*')
-        .where('server_id', request.query.server_id)
-        .then(responseData => {
-            response.status(200).send(responseData);
-        })
-        .catch(error => {
-            response.status(500).send(error);
-        });
-    } else {
-        knex(request.params.table_name)
-        .select('*')
-        .then(responseData => {
-            response.status(200).send(responseData);
-        })
-        .catch(error => {
-            response.status(500).send(error);
-        });
-    }
+  if (request.query.id !== undefined) {
+    knex(request.params.table_name)
+      .select('*')
+      .where('id', request.query.id)
+      .then((responseData) => {
+        response.status(200).send(responseData);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  } else if (request.query.server_id !== undefined) {
+    knex(request.params.table_name)
+      .select('*')
+      .where('server_id', request.query.server_id)
+      .then((responseData) => {
+        response.status(200).send(responseData);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  } else {
+    knex(request.params.table_name)
+      .select('*')
+      .then((responseData) => {
+        response.status(200).send(responseData);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  }
 });
 
 app.post('/data/:table_name', (request, response) => {
-    knex(request.params.table_name)
+  knex(request.params.table_name)
     .insert(request.body)
     .then(() => {
-        response.status(200).send(`${request.params.table_name} created`);
+      response.status(200).send(`${request.params.table_name} created`);
     })
-    .catch(error => {
-        response.status(500).send(error);
+    .catch((error) => {
+      response.status(500).send(error);
     });
 });
 
 app.patch('/data/:table_name', (request, response) => {
-    if(request.query.id !== undefined) {
-        knex(request.params.table_name)
-        .where('id', request.query.id)
-        .update(request.body)
-        .then(() => {
-            response.status(200).send(`${request.params.table_name} updated`);
-        })
-        .catch(error => {
-            response.status(500).send(error);
-        });
-    } else {
-        response.status(500).send('No id specified');
-    }
+  if (request.query.id !== undefined) {
+    knex(request.params.table_name)
+      .where('id', request.query.id)
+      .update(request.body)
+      .then(() => {
+        response.status(200).send(`${request.params.table_name} updated`);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  } else {
+    response.status(500).send('No id specified');
+  }
 });
 
 app.delete('/data/:table_name', (request, response) => {
-    if(['signal', 'save'].includes(request.params.table_name) && request.query.id !== undefined) {
-        knex(request.params.table_name)
-        .where('id', request.query.id)
-        .del()
-        .then(() => {
-            response.status(200).send(`${request.params.table_name} deleted`);
-        })
-        .catch(error => {
-            response.status(500).send(error);
-        });
-    } else {
-        response.status(500).send('No id specified, or table name is not valid');
-    }
+  if (
+    ['signal', 'save'].includes(request.params.table_name) &&
+    request.query.id !== undefined
+  ) {
+    knex(request.params.table_name)
+      .where('id', request.query.id)
+      .del()
+      .then(() => {
+        response.status(200).send(`${request.params.table_name} deleted`);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  } else {
+    response.status(500).send('No id specified, or table name is not valid');
+  }
 });
 
 module.exports = app;
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,27 +1,25 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import config from './config';
 import { Header, Body } from './components';
 
-const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
+const ApiUrl = config[process.env.REACT_APP_NODE_ENV || 'development'].apiUrl;
 
 function App() {
-
-  let [names, setNames] = useState([ ]);
+  let [names, setNames] = useState([]);
 
   useEffect(() => {
-    fetch(ApiUrl + "/authors")
-      .then(response => response.json())
-      .then(data => setNames(data))
-      .catch(err => console.log(err))
+    fetch(ApiUrl + '/authors')
+      .then((response) => response.json())
+      .then((data) => setNames(data))
+      .catch((err) => console.log(err));
   }, []);
-
 
   return (
     <div>
       <Header />
       <Body />
-      App is running - good work: 
-      { names.map(author => author.firstName + " ")}
+      App is running - good work:
+      {names.map((author) => author.first_name + ' ')}
     </div>
   );
 }
