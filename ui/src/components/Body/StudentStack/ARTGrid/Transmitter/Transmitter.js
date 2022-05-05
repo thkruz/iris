7deff08a5bd99a212299a00a9270a7f4f6de7bcb
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-05-05 12:55:11 -0400

    use production build on heroku

--- ui/src/components/Body/StudentStack/ARTGrid/Transmitter/Transmitter.js
+++ ui/src/components/Body/StudentStack/ARTGrid/Transmitter/Transmitter.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import { TxModem } from './TxModem/TxModem';
 
-
 function Transmitter() {
   const units = [1, 2, 3, 4];
   return units.map((x, index) => (
-    <Grid item xs={3}>
+    <Grid key={index} item xs={3}>
       <TxModem unit={x} />
     </Grid>
   ));
