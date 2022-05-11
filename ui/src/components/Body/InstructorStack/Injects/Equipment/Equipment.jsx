cb6beeb030acd8e68da5a26b784a45f3169e5259
bjhufstetler <bjhufstetler@gmail.com>
2022-05-11 09:21:29 -0700

    chore: fixes tx seed

--- ui/src/components/Body/InstructorStack/Injects/Equipment/Equipment.jsx
+++ ui/src/components/Body/InstructorStack/Injects/Equipment/Equipment.jsx
@@ -4,62 +4,9 @@ import React from 'react';
 //import { AstroTheme } from '../../../../../themes/AstroTheme';
 
 export const Equipment = () => {
-    /*
-    const tmpReceiver = [
-        
-    ];
-    const tmpTransmitter = [
-        
-    ];
-    const tmpSpecA = [
-        ,
-    ]
-    
-    const tmpAntenna = [
-        ,
-    ]
-
-    const tmpRxOps = [
-        {id: 1, id_server: 1, id_team: 1, unit: 1, operational: true},
-        {id: 2, id_server: 1, id_team: 1, unit: 2, operational: true},
-        {id: 3, id_server: 1, id_team: 1, unit: 3, operational: true},
-        {id: 4, id_server: 1, id_team: 1, unit: 4, operational: true},
-        {id: 5, id_server: 1, id_team: 2, unit: 1, operational: true},
-        {id: 6, id_server: 1, id_team: 2, unit: 2, operational: true},
-        {id: 7, id_server: 1, id_team: 2, unit: 3, operational: true},
-        {id: 8, id_server: 1, id_team: 2, unit: 4, operational: true},
-        {id: 9, id_server: 1, id_team: 3, unit: 1, operational: true},
-        {id: 10, id_server: 1, id_team: 3, unit: 2, operational: true},
-        {id: 11, id_server: 1, id_team: 3, unit: 3, operational: true},
-        {id: 12, id_server: 1, id_team: 3, unit: 4, operational: true},
-        {id: 13, id_server: 1, id_team: 4, unit: 1, operational: true},
-        {id: 14, id_server: 1, id_team: 4, unit: 2, operational: true},
-        {id: 15, id_server: 1, id_team: 4, unit: 3, operational: true},
-        {id: 16, id_server: 1, id_team: 4, unit: 4, operational: true}
-    ];
-
-    const tmpTxOps = [
-        {id: 1, id_server: 1, id_team: 1, unit: 1, operational: true},
-        {id: 2, id_server: 1, id_team: 1, unit: 2, operational: true},
-        {id: 3, id_server: 1, id_team: 1, unit: 3, operational: true},
-        {id: 4, id_server: 1, id_team: 1, unit: 4, operational: true},
-        {id: 5, id_server: 1, id_team: 2, unit: 1, operational: true},
-        {id: 6, id_server: 1, id_team: 2, unit: 2, operational: true},
-        {id: 7, id_server: 1, id_team: 2, unit: 3, operational: true},
-        {id: 8, id_server: 1, id_team: 2, unit: 4, operational: true},
-        {id: 9, id_server: 1, id_team: 3, unit: 1, operational: true},
-        {id: 10, id_server: 1, id_team: 3, unit: 2, operational: true},
-        {id: 11, id_server: 1, id_team: 3, unit: 3, operational: true},
-        {id: 12, id_server: 1, id_team: 3, unit: 4, operational: true},
-        {id: 13, id_server: 1, id_team: 4, unit: 1, operational: true},
-        {id: 14, id_server: 1, id_team: 4, unit: 2, operational: true},
-        {id: 15, id_server: 1, id_team: 4, unit: 3, operational: true},
-        {id: 16, id_server: 1, id_team: 4, unit: 4, operational: true}
-    ];
-    const tmpTeams
-    const txData = useTx();
+    //const txData = useTx();
     //const theme = AstroTheme;
-*/
+
     return (
         <>
             {}
