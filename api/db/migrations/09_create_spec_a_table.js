3f64a2f913ab2b73b11b7760847e533eed07d0f8
filmo003 <collin.gilmore47@gmail.com>
2022-05-11 14:42:23 -0400

    fix: :ambulance: fix db migrations and seed data

--- api/db/migrations/09_create_spec_a_table.js
+++ api/db/migrations/09_create_spec_a_table.js
@@ -10,15 +10,13 @@ exports.up = function(knex) {
         table.integer('team_id').notNullable();
         table.foreign('team_id').references('team.id');
         table.integer('unit').notNullable();
-        table.integer('modem_number').notNullable();
         table.boolean('operational').defaultTo(false);
         table.double('frequency').notNullable();
         table.double('span').notNullable();
-        table.string('trace1').notNullable();
-        table.string('trace2').notNullable();
-        table.string('trace3').notNullable();
         table.double('marker1freq').notNullable();
         table.double('marker2freq').notNullable();
+        table.integer('number').notNullable();
+        table.boolean('trace').notNullable();
     });
 };
 
