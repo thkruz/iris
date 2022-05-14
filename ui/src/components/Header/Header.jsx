3c9ec18b8dcf6af55b3942e21b94c366fbf17d54
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-05-13 22:07:35 -0400

    feat: :sparkles: add link back to home page

--- ui/src/components/Header/Header.jsx
+++ ui/src/components/Header/Header.jsx
@@ -5,38 +5,36 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Logout from '@mui/icons-material/Logout';
-import { AstroTheme } from '../../themes/AstroTheme'
-import './Header.css'
+import { AstroTheme } from '../../themes/AstroTheme';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   const auth = true; //TODO: get auth context
   const theme = AstroTheme;
 
   const handleLogout = () => {
-    console.log("logging out")
-  }
-    return (
-      <>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar sx={{ backgroundColor: theme.palette.primary.dark }}>
+    console.log('logging out');
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar className={'appBar'} position='static'>
+          <Toolbar sx={{ backgroundColor: theme.palette.primary.dark }}>
+            <Link to='/'>
               <img src='/patch.png' alt='patch.png' height='80px'></img>
-              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-                Iris Space Electronic Warfare Sandbox
-              </Typography>
-              {auth &&
-                <IconButton
-                  size="large"
-                  onClick={handleLogout}
-                  color="inherit"
-                  >
-                    <Logout />
-                </IconButton>
-              }
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </>
-    );
-  }
-  
+            </Link>
+            <Typography variant='h4' component='div' sx={{ flexGrow: 1 }}>
+              Iris Space Electronic Warfare Sandbox
+            </Typography>
+            {auth && (
+              <IconButton size='large' onClick={handleLogout} color='inherit'>
+                <Logout />
+              </IconButton>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
