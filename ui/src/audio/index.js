b61cfabd365897ff0c94e998c26def074de6c0c9
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-06-12 21:02:12 -0400

    feat: :loud_sound: add sounds

---
+++ ui/src/audio/index.js
@@ -0,0 +1,8 @@
+import switchSound from './switch.flac';
+import breakerSound from './breaker.wav';
+import errorSound from './error.flac';
+import raytubeOnSound from './raytubeOn.wav';
+import selectSound from './select.wav';
+import staticSound from './static.wav';
+
+export { switchSound, breakerSound, errorSound, raytubeOnSound, selectSound, staticSound };
